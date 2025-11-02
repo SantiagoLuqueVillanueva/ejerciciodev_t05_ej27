33c927c9ce93ea40af3e46a0842fc2ba41b168b5
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-02 12:05:09 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        int numeroIntroducido = Integer.parseInt(System.console().readLine("Introduzca un número entero mayor que 1: "));
+
+        int contador = 0;
+        int suma = 0;
+        int sol = 3;
+        
+        while (sol < numeroIntroducido) {
+            System.out.print(sol + " ");
+            contador++;
+            suma += sol; 
+            sol += 3;
+        }
+        System.out.println();
+
+        System.out.printf("Desde 1 hasta %d hay %d múltiplos de 3 y suman %d", numeroIntroducido, contador, suma);
+    }   
+
 }
